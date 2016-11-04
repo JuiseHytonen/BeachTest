b3b42fd8149aa8e73a928f749cfa706b4d846d61
S1400\gamer <juise.hytonen@gmail.com>
2016-11-04 10:49:26 +0200

    initial commit
    initi

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour {
+
+    public ViveController RightGameController;
+    public ParticleSystem Particles;
+    public MeshCollider CaveCollider;
+    private bool _attachedToLight = false;
+    private bool _isHit = false;
+   
+	// Use this for initialization
+	void Start () {
+        Debug.Log("starting sphere controller" + Time.realtimeSinceStartup);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if ((RightGameController.transform.position - this.transform.position).magnitude < .1f || _attachedToLight)
+        {
+            // First time we´re close attach for good.
+            _attachedToLight = true;
+            if (RightGameController.IsInValidSpace)
+            {
+                this.transform.position = RightGameController.transform.position;
+                Particles.Stop();
+            }
+            else
+            {
+                Particles.Play();
+            }
+        }
+    }
+    /**
+    void OnTriggerEnter(Collider other)
+    {
+        if(other == CaveCollider)
+        {
+            Debug.Log("hit");
+            _isHit = true;
+            Particles.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other == CaveCollider)
+        {
+            Debug.Log("exit");
+            _isHit = false;
+            Particles.Stop();
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("hit");
+        //Destroy(gameObject);
+    }
+    **/
+}
---
+++ Assets/Scripts/ViveController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveController : MonoBehaviour {
+
+    public MeshCollider CaveCollider;
+    public bool IsInValidSpace;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other == CaveCollider)
+        {
+            if(IsInValidSpace)
+            {
+                Debug.Log("Vivecontroller hit cave collider" + Time.realtimeSinceStartup);
+            }
+            IsInValidSpace = false;
+            
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other == CaveCollider)
+        {
+            if (!IsInValidSpace)
+            {
+                Debug.Log("Vivecontroller left cave collider" + Time.realtimeSinceStartup);
+            }
+
+            IsInValidSpace = true;
+        }
+    }
+}
